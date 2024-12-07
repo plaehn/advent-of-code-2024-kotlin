a7735312009be27aa92b693b35025d6aca7e5fd0
Oliver Plaehn <oliver.plaehn@gmail.com>
2024-12-07 17:14:25 +0100

    Day 7: Use fold

--- src/main/kotlin/org/plaehn/adventofcode/Day07.kt
+++ src/main/kotlin/org/plaehn/adventofcode/Day07.kt
@@ -2,30 +2,34 @@ package org.plaehn.adventofcode
 
 import org.plaehn.adventofcode.common.concat
 
-class Day07(
-    private val equations: List<Equation>,
-    private val useConcat: Boolean
-) {
+typealias LongOperation = (Long, Long) -> Long
 
-    fun solve(): Long =
-        equations.sumOf { equation -> equation.solve(useConcat) }
+class Day07(
+    private val equations: List<Equation>
+) {
+    fun solvePart1(): Long =
+        solve(listOf(Long::plus, Long::times))
+
+    fun solvePart2(): Long =
+        solve(listOf(Long::plus, Long::times, Long::concat))
+
+    private fun solve(operations: List<LongOperation>): Long =
+        equations.sumOf { equation -> equation.solve(operations) }
 
     data class Equation(
         val testValue: Long,
         val operands: List<Long>
     ) {
-        fun solve(useConcat: Boolean): Long =
-            compute(operands.first(), operands.drop(1), useConcat)
 
-        private fun compute(result: Long, operands: List<Long>, useConcat: Boolean): Long =
-            if (operands.isEmpty() || result >= testValue)
-                result
-            else
-                listOf(Long::plus, Long::times, Long::concat)
-                    .filter { useConcat || it != Long::concat }
-                    .map { operation -> compute(operation(result, operands.first()), operands.drop(1), useConcat) }
-                    .find { it == testValue }
-                    ?: 0
+        fun solve(operations: List<LongOperation>): Long =
+            operands
+                .drop(1)
+                .fold(listOf(operands.first())) { acc, operand ->
+                    operations
+                        .flatMap { operation -> acc.map { operation(it, operand) } }
+                        .filter { it <= testValue }
+                }
+                .find { it == testValue } ?: 0
 
         companion object {
             fun fromInput(input: String): Equation {
@@ -39,7 +43,7 @@ class Day07(
     }
 
     companion object {
-        fun fromInput(lines: List<String>, useConcat: Boolean = false) =
-            Day07(lines.map { Equation.fromInput(it) }, useConcat)
+        fun fromInput(lines: List<String>) =
+            Day07(lines.map { Equation.fromInput(it) })
     }
 }
--- src/test/kotlin/org/plaehn/adventofcode/Day07Test.kt
+++ src/test/kotlin/org/plaehn/adventofcode/Day07Test.kt
@@ -11,27 +11,27 @@ class Day07Test {
     fun `Part 1 Test Input`() {
         val lines = this::class.java.readLines("07_test.txt")
 
-        assertThat(Day07.fromInput(lines).solve()).isEqualTo(3749)
+        assertThat(Day07.fromInput(lines).solvePart1()).isEqualTo(3749)
     }
 
     @Test
     fun `Part 1 Puzzle Input`() {
         val lines = this::class.java.readLines("07.txt")
 
-        assertThat(Day07.fromInput(lines).solve()).isEqualTo(5512534574980)
+        assertThat(Day07.fromInput(lines).solvePart1()).isEqualTo(5512534574980)
     }
 
     @Test
     fun `Part 2 Test Input`() {
         val lines = this::class.java.readLines("07_test.txt")
 
-        assertThat(Day07.fromInput(lines, useConcat = true).solve()).isEqualTo(11387)
+        assertThat(Day07.fromInput(lines).solvePart2()).isEqualTo(11387)
     }
 
     @Test
     fun `Part 2 Puzzle Input`() {
         val lines = this::class.java.readLines("07.txt")
 
-        assertThat(Day07.fromInput(lines, useConcat = true).solve()).isEqualTo(328790210468594)
+        assertThat(Day07.fromInput(lines).solvePart2()).isEqualTo(328790210468594)
     }
 }
