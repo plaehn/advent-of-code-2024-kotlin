4776c78fba1494f7c259df9bac1a070182cf8b35
oliver.plaehn <oliver.plaehn@gmail.com>
2024-12-22 17:29:16 +0100

    Day 21, part 1 and 2

--- src/main/kotlin/org/plaehn/adventofcode/Day21.kt
+++ src/main/kotlin/org/plaehn/adventofcode/Day21.kt
@@ -1,13 +1,94 @@
 package org.plaehn.adventofcode
 
-class Day21(codes: List<String>) {
+import org.plaehn.adventofcode.common.Coord
+import org.plaehn.adventofcode.common.Coord.Companion.DOWN
+import org.plaehn.adventofcode.common.Coord.Companion.LEFT
+import org.plaehn.adventofcode.common.Coord.Companion.RIGHT
+import org.plaehn.adventofcode.common.Coord.Companion.UP
+import org.plaehn.adventofcode.common.Matrix
+import java.util.*
 
-    fun solvePart1(): Int {
-        return 0
+class Day21(private val codes: List<String>) {
+
+    private val numericKeypad = Matrix.fromRows(
+        listOf("789", "456", "123", "#0A").map { it.toCharArray().toList() }, '#'
+    ).toMap().toMutableMap().apply { remove(Coord(0, 3)) }
+
+    private val directionalKeypad = Matrix.fromRows(
+        listOf("#^A", "<v>").map { it.toCharArray().toList() }, '#'
+    ).toMap().toMutableMap().apply { remove(Coord(0, 0)) }
+
+    private val numericPathsWithLowestCost = numericKeypad.allPathsWithLowestCost()
+
+    private val directionalPathsWithLowestCost = directionalKeypad.allPathsWithLowestCost()
+
+    private fun Map<Coord, Char>.allPathsWithLowestCost(): Map<Pair<Char, Char>, List<String>> =
+        keys.allPairs()
+            .associate {
+                (getValue(it.first) to getValue(it.second)) to findLowestCostPaths(it.first, it.second)
+            }
+
+    private fun Map<Coord, Char>.findLowestCostPaths(start: Coord, end: Coord): List<String> {
+        val queue = PriorityQueue<Pair<List<Coord>, Int>>(compareBy { it.second })
+            .apply { add(listOf(start) to 0) }
+        val seen = mutableMapOf<Coord, Int>()
+        var costAtGoal: Int? = null
+        val allPaths: MutableList<String> = mutableListOf()
+
+        while (queue.isNotEmpty()) {
+            val (path, cost) = queue.poll()
+            val location = path.last()
+
+            if (costAtGoal != null && cost > costAtGoal) {
+                return allPaths
+            } else if (path.last() == end) {
+                costAtGoal = cost
+                allPaths.add(path.zipWithNext().map { (from, to) -> from.diffToChar(to) }.joinToString("") + "A")
+            } else if (seen.getOrDefault(location, Int.MAX_VALUE) >= cost) {
+                seen[location] = cost
+                location
+                    .neighbors()
+                    .filter { it in keys }
+                    .forEach { queue.add(path + it to cost + 1) }
+            }
+        }
+        return allPaths
     }
 
-    fun solvePart2(): Int {
-        return 0
+    private fun Coord.diffToChar(other: Coord): Char =
+        when (val result = other - this) {
+            UP -> '^'
+            RIGHT -> '>'
+            DOWN -> 'v'
+            LEFT -> '<'
+            else -> throw IllegalArgumentException("Invalid direction $result")
+        }
+
+    private fun <T> Collection<T>.allPairs(): List<Pair<T, T>> =
+        flatMap { left ->
+            map { right -> left to right }
+        }
+
+    fun solve(depth: Int): Long {
+
+        return codes.sumOf { code -> findCost(code, depth) * code.dropLast(1).toLong() }
     }
+
+    private fun findCost(
+        code: String,
+        depth: Int,
+        transitions: Map<Pair<Char, Char>, List<String>> = numericPathsWithLowestCost,
+        cache: MutableMap<Pair<String, Int>, Long> = mutableMapOf()
+    ): Long =
+        cache.getOrPut(code to depth) {
+            "A$code".zipWithNext().sumOf { transition ->
+                val paths: List<String> = transitions.getValue(transition)
+                if (depth == 0) {
+                    paths.minOf { it.length }.toLong()
+                } else {
+                    paths.minOf { path -> findCost(path, depth - 1, directionalPathsWithLowestCost, cache) }
+                }
+            }
+        }
 }
 
--- src/test/kotlin/org/plaehn/adventofcode/Day21Test.kt
+++ src/test/kotlin/org/plaehn/adventofcode/Day21Test.kt
@@ -11,28 +11,28 @@ class Day21Test {
     fun `Part 1 Test Input`() {
         val day21 = Day21(readLines("21_test.txt"))
 
-        assertThat(day21.solvePart1()).isEqualTo(126384)
+        assertThat(day21.solve(depth = 2)).isEqualTo(126384)
     }
 
     @Test
     fun `Part 1 Puzzle Input`() {
         val day21 = Day21(readLines("21.txt"))
 
-        assertThat(day21.solvePart1()).isEqualTo(-1)
+        assertThat(day21.solve(depth = 2)).isEqualTo(-1)
     }
 
     @Test
     fun `Part 2 Test Input`() {
         val day21 = Day21(readLines("21_test.txt"))
 
-        assertThat(day21.solvePart2()).isEqualTo(-1)
+        assertThat(day21.solve(depth = 25)).isEqualTo(154115708116294)
     }
 
     @Test
     fun `Part 2 Puzzle Input`() {
         val day21 = Day21(readLines("21.txt"))
 
-        assertThat(day21.solvePart2()).isEqualTo(-1)
+        assertThat(day21.solve(depth = 25)).isEqualTo(223902935165512)
     }
 
     private fun readLines(resource: String) = this::class.java.readLines(resource)
