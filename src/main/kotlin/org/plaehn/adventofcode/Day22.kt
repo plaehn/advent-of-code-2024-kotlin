efd5017c2fc8840b1d2425a6c9fe354dd9f2f9b4
oliver.plaehn <oliver.plaehn@gmail.com>
2024-12-24 14:02:45 +0100

    Day 22, part 2: slow solution (15 secs)

--- src/main/kotlin/org/plaehn/adventofcode/Day22.kt
+++ src/main/kotlin/org/plaehn/adventofcode/Day22.kt
@@ -3,13 +3,34 @@ package org.plaehn.adventofcode
 class Day22(private val secretNumbers: List<Long>) {
 
     fun solvePart1(): Long =
-        secretNumbers.map { computeListsOfSecretNumbers(it) }.sumOf { it.last() }
+        secretNumbers.map { computeSecretNumbers(it) }.sumOf { it.last() }
 
-    fun solvePart2(): Long {
-        return 0
+    fun solvePart2(): Int {
+        val differenceLists: List<List<Pair<Int, Int>>> =
+            secretNumbers
+                .map { computeSecretNumbers(it) }
+                .map { secretNumbers -> secretNumbers.map { (it % 10).toInt() } }
+                .map { secretNumbers -> secretNumbers.zipWithNext { prev, current -> current to (current - prev) } }
+
+        val quadruple2BananasForAllLists: List<Map<List<Int>, Int>> = differenceLists.map { differenceList ->
+            val quadruple2Bananas: MutableMap<List<Int>, Int> = mutableMapOf()
+            differenceList
+                .windowed(size = 4)
+                .forEach { window: List<Pair<Int, Int>> ->
+                    quadruple2Bananas.getOrPut(window.map { it.second }) { window.last().first }
+                }
+            quadruple2Bananas.toMap()
+        }
+        val quadruple2SummedBananas: Map<List<Int>, Int> =
+            quadruple2BananasForAllLists.reduce { prev: Map<List<Int>, Int>, current: Map<List<Int>, Int> ->
+                (prev.keys + current.keys).associateWith { key ->
+                    prev.getOrDefault(key, 0) + current.getOrDefault(key, 0)
+                }
+            }
+        return quadruple2SummedBananas.maxBy { it.value }.value
     }
 
-    private fun computeListsOfSecretNumbers(secret: Long): List<Long> =
+    private fun computeSecretNumbers(secret: Long): List<Long> =
         sequence {
             var current = secret
             (0..2000).map {
--- src/test/kotlin/org/plaehn/adventofcode/Day22Test.kt
+++ src/test/kotlin/org/plaehn/adventofcode/Day22Test.kt
@@ -9,7 +9,7 @@ class Day22Test {
 
     @Test
     fun `Part 1 Test Input`() {
-        val day22 = Day22(readLinesAsLongs("22_test.txt"))
+        val day22 = Day22(listOf(1, 10, 100, 2024))
 
         assertThat(day22.solvePart1()).isEqualTo(37327623)
     }
@@ -23,16 +23,16 @@ class Day22Test {
 
     @Test
     fun `Part 2 Test Input`() {
-        val day22 = Day22(readLinesAsLongs("22_test.txt"))
+        val day22 = Day22(listOf(1, 2, 3, 2024))
 
-        assertThat(day22.solvePart2()).isEqualTo(-1)
+        assertThat(day22.solvePart2()).isEqualTo(23)
     }
 
     @Test
     fun `Part 2 Puzzle Input`() {
         val day22 = Day22(readLinesAsLongs("22.txt"))
 
-        assertThat(day22.solvePart2()).isEqualTo(-1)
+        assertThat(day22.solvePart2()).isEqualTo(2242)
     }
 
     private fun readLinesAsLongs(resource: String) =
