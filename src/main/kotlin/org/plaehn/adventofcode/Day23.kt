ae92c351f8bd9b88da8728196ee2f5c4f0892c1f
plaehn <oliver.plaehn@web.de>
2024-12-24 21:04:08 +0100

    Day 23, part 1

---
+++ src/main/kotlin/org/plaehn/adventofcode/Day23.kt
@@ -0,0 +1,40 @@
+package org.plaehn.adventofcode
+
+import com.google.common.graph.GraphBuilder
+import org.plaehn.adventofcode.common.combinations
+
+class Day23(private val connections: List<String>) {
+
+    fun solvePart1(): Int =
+        buildConnectionGraph().run {
+            nodes()
+                .filter { it.startsWith("t") }
+                .flatMap { node ->
+                    adjacentNodes(node)
+                        .combinations(ofSize = 2)
+                        .filter { otherNodes -> hasEdgeConnecting(otherNodes.first(), otherNodes.last()) }
+                        .map { otherNodes -> otherNodes + node }
+                }
+                .toSet()
+                .size
+        }
+
+    private fun buildConnectionGraph() =
+        GraphBuilder
+            .undirected()
+            .expectedNodeCount(connections.size * 2)
+            .immutable<String>()
+            .apply {
+                connections.forEach { connection ->
+                    val (lhs, rhs) = connection.split("-")
+                    addNode(lhs)
+                    addNode(rhs)
+                    putEdge(lhs, rhs)
+                }
+            }
+            .build()
+
+    fun solvePart2(): Long {
+        return 0
+    }
+}
---
+++ src/test/kotlin/org/plaehn/adventofcode/Day23Test.kt
@@ -0,0 +1,39 @@
+package org.plaehn.adventofcode
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import org.junit.jupiter.api.Test
+import org.plaehn.adventofcode.common.readLines
+
+class Day23Test {
+
+    @Test
+    fun `Part 1 Test Input`() {
+        val day23 = Day23(readLines("23_test.txt"))
+
+        assertThat(day23.solvePart1()).isEqualTo(7)
+    }
+
+    @Test
+    fun `Part 1 Puzzle Input`() {
+        val day23 = Day23(readLines("23.txt"))
+
+        assertThat(day23.solvePart1()).isEqualTo(1054)
+    }
+
+    @Test
+    fun `Part 2 Test Input`() {
+        val day23 = Day23(readLines("23_test.txt"))
+
+        assertThat(day23.solvePart2()).isEqualTo(-1)
+    }
+
+    @Test
+    fun `Part 2 Puzzle Input`() {
+        val day23 = Day23(readLines("23.txt"))
+
+        assertThat(day23.solvePart2()).isEqualTo(-1)
+    }
+
+    private fun readLines(resource: String) = this::class.java.readLines(resource)
+}
