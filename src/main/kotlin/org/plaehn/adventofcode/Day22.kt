82e776f7fccfd2f6e703805431b3f861a475a9c4
oliver.plaehn <oliver.plaehn@gmail.com>
2024-12-24 12:49:35 +0100

    Day 22, part 1: refactor for part 2

--- src/main/kotlin/org/plaehn/adventofcode/Day22.kt
+++ src/main/kotlin/org/plaehn/adventofcode/Day22.kt
@@ -3,14 +3,26 @@ package org.plaehn.adventofcode
 class Day22(private val secretNumbers: List<Long>) {
 
     fun solvePart1(): Long =
-        secretNumbers.sumOf { computeNthSecretNumber(it, 2000) }
+        secretNumbers.map { computeListsOfSecretNumbers(it) }.sumOf { it.last() }
 
-    private fun computeNthSecretNumber(current: Long, nth: Int): Long {
-        if (nth == 0) return current
-        var next = (current * 64).mix(current).prune()
+    fun solvePart2(): Long {
+        return 0
+    }
+
+    private fun computeListsOfSecretNumbers(secret: Long): List<Long> =
+        sequence {
+            var current = secret
+            (0..2000).map {
+                this.yield(current)
+                current = current.computeNextSecretNumber()
+            }
+        }.toList()
+
+    private fun Long.computeNextSecretNumber(): Long {
+        var next = (this * 64).mix(this).prune()
         next = (next / 32).mix(next).prune()
         next = (next * 2048).mix(next).prune()
-        return computeNthSecretNumber(next, nth - 1)
+        return next
     }
 
     private fun Long.mix(secret: Long): Long =
@@ -18,8 +30,4 @@ class Day22(private val secretNumbers: List<Long>) {
 
     private fun Long.prune(): Long =
         this % 16777216
-
-    fun solvePart2(): Long {
-        return 0
-    }
 }
